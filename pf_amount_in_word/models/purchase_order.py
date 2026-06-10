30548b915c34c724e8c86178fc62fbf100f62057
Prefortune-Odoo <153276923+Prefortune-Odoo@users.noreply.github.com>
2026-06-11 04:24:21 +0530

    Add files via upload

---
+++ pf_amount_in_word/models/purchase_order.py
@@ -0,0 +1,21 @@
+from odoo import models, fields, api
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    amount_in_words = fields.Char(
+        string="Amount In Words",
+        compute='_compute_amount_in_words',
+        store=True,
+        help="Total amount in words"
+    )
+
+    @api.depends('amount_total', 'currency_id')
+    def _compute_amount_in_words(self):
+        for order in self:
+            if order.currency_id:
+                order.amount_in_words = order.currency_id.amount_to_text(order.amount_total)
+            else:
+                order.amount_in_words = ""
+
+
