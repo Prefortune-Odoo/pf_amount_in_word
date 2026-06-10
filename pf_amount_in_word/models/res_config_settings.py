3894f8901835b8f4c26cda76b213c7917f9410ac
Prefortune-Odoo <153276923+Prefortune-Odoo@users.noreply.github.com>
2026-06-11 04:14:18 +0530

    Delete pf_amount_in_word directory

--- pf_amount_in_word/models/res_config_settings.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
-
-    l10n_in_gsp = fields.Boolean(string="L10n In GSP", help="Dummy field to fix OwlError in settings")
