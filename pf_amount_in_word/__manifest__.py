0dc121010f5d87d845fc287174eb2d5fabade1a2
Prefortune-Odoo <153276923+Prefortune-Odoo@users.noreply.github.com>
2026-06-11 04:28:59 +0530

    Update __manifest__.py

--- pf_amount_in_word/__manifest__.py
+++ pf_amount_in_word/__manifest__.py
@@ -19,7 +19,7 @@
     'maintainer': 'Prefortune Technologies LLP',
     "support": "odoo@prefortune.com",
     'currency': 'EUR',
-	'price': '',
+	'price': '0.00',
     'depends': ['sale', 'purchase', 'account'],
     'data': [
         'views/sale_order_views.xml',
